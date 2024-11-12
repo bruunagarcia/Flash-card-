9e7deb6dfc8f7eab7a139b581c0a59996fc2c0df
bruunagarcia <146857241+bruunagarcia@users.noreply.github.com>
2024-11-12 08:01:17 -0300

    Add files via upload

---
+++ Flash-card--main/Flash-card--main/Flash-card--main/flashCard/assets/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
